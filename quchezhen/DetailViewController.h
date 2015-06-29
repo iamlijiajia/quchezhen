8f3233036a1bc4611301ff9c0e22cf943c53e8e6
jiajia <iamlijiajia@sina.com>
2015-06-29 11:10:32 +0800

    Initial Commit

---
+++ quchezhen/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  quchezhen
+//
+//  Created by lijiajia on 15/6/29.
+//  Copyright (c) 2015年 lijiajia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
